ea63179aa5829148c3cdb420fcd732b38a558bf0
Viet Xuan Pham <viet.pham@asnet.com.vn>
2025-05-16 15:23:36 +0700

    #3 Feat: Configuration extensions for project

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation changes
+        'style',    // Code style changes (formatting, etc)
+        'refactor', // Code refactoring
+        'test',     // Adding or modifying tests
+        'chore',    // Maintenance tasks
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+}; 

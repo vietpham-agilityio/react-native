675c9f6f632d0ca9bfe024c6c0f73f115aeaf2e6
Viet Xuan Pham <viet.pham@asnet.com.vn>
2025-05-19 15:31:13 +0700

    #18 Chore: Implement mock data constant

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = {
           '@utils': './src/utils',
           '@hooks': './src/hooks',
           '@types': './src/types',
+          '@mock': './src/mock',
         },
       },
     ],
---
+++ src/mock/data.ts
@@ -0,0 +1,80 @@
+export const AUTHORS_DATA_MOCK = [
+  {
+    id: '1',
+    image: require('@assets/images/authors/harper-lee.webp'),
+    name: 'Harper Lee',
+    role: 'Writer',
+  },
+
+  {
+    id: '2',
+    image: require('@assets/images/authors/jose-mauro-de.webp'),
+    name: 'Jose Mauro',
+    role: 'Novelist',
+  },
+  {
+    id: '3',
+    image: require('@assets/images/authors/murakami.webp'),
+    name: 'H.Murakami',
+    role: 'Novelist',
+  },
+  {
+    id: '4',
+    image: require('@assets/images/authors/nhat-anh.webp'),
+    name: 'Nhat Anh',
+    role: 'Writer',
+  },
+];
+
+export const BOOKS_DATA_MOCK = [
+  {
+    id: '1',
+    title: 'Her Come Tomorrow',
+    author: 'Nhat Anh',
+    image: require('@assets/images/books/her-come-tomorrow.webp'),
+    description: 'A novel about the American Dream',
+    price: 8.99,
+    rating: 4,
+    reviews: 1200,
+  },
+  {
+    id: '2',
+    title: 'Kafka on the Beach',
+    author: 'Haruki Murakami',
+    image: require('@assets/images/books/kafka-on-beach.webp'),
+    description: 'A novel about the American Dream',
+    price: 10.99,
+    rating: 5,
+    reviews: 380,
+  },
+  {
+    id: '3',
+    title: 'Kill a Mockingbird',
+    author: 'Harper Lee',
+    image: require('@assets/images/books/kill-a-mocking.webp'),
+    description: 'A novel about the American Dream',
+    price: 12.99,
+    rating: 4,
+    reviews: 2340,
+  },
+  {
+    id: '4',
+    title: 'My Orange Tree',
+    author: 'Jose Mauro Torre',
+    image: require('@assets/images/books/my-orange-tree.webp'),
+    description: 'A novel about the American Dream',
+    price: 13.3,
+    rating: 4,
+    reviews: 872,
+  },
+  {
+    id: '5',
+    title: 'Norwegian Wood',
+    author: 'Haruki Murakami',
+    image: require('@assets/images/books/norwegian-wood.webp'),
+    description: 'A novel about the American Dream',
+    price: 12.99,
+    rating: 5,
+    reviews: 6720,
+  },
+];

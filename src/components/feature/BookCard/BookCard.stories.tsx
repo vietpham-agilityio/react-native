cc7e6e48c7e2b17a3fffcf21745abc5c26a5cb2c
Viet Xuan Pham <viet.pham@asnet.com.vn>
2025-05-19 16:58:17 +0700

    #11 Feature: Implement BookCard feature component

---
+++ src/components/feature/BookCard/BookCard.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+// Components
+import { ScrollView, View } from 'react-native';
+import BookCard from './index';
+
+// Mock
+import { BOOKS_DATA_MOCK } from '@/mock/data';
+
+const meta = {
+  title: 'Feature/BookCard',
+  component: BookCard,
+  args: {
+    ...BOOKS_DATA_MOCK[0],
+    onPress: () => {},
+  },
+  decorators: [
+    Story => (
+      <View style={{ padding: 16, alignItems: 'center', width: '100%' }}>
+        <Story />
+      </View>
+    ),
+  ],
+} satisfies Meta<typeof BookCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const BookCardSingle: Story = {
+  render: () => <BookCard {...BOOKS_DATA_MOCK[0]} onPress={() => {}} />,
+};
+
+export const BookCardList: Story = {
+  render: () => (
+    <ScrollView horizontal>
+      {BOOKS_DATA_MOCK.map(book => (
+        <View style={{ marginRight: 16 }} key={book.id}>
+          <BookCard
+            image={book.image}
+            title={book.title}
+            price={book.price}
+            onPress={() => {}}
+          />
+        </View>
+      ))}
+    </ScrollView>
+  ),
+};
---
+++ src/components/feature/BookCard/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  ImageSourcePropType,
+  TouchableOpacity,
+} from 'react-native';
+
+// Components
+import { Typography } from '@/components/common';
+
+// Theme
+import { colors, radius } from '@/theme';
+
+interface BookCardProps {
+  image: ImageSourcePropType;
+  title: string;
+  price: number;
+  onPress: () => void;
+}
+
+const BookCard = ({ image, title, price, onPress }: BookCardProps) => (
+  <TouchableOpacity
+    style={styles.container}
+    activeOpacity={0.7}
+    onPress={onPress}
+    accessibilityRole="button">
+    <Image
+      source={image}
+      style={styles.cover}
+      accessibilityRole="image"
+      testID="book-image"
+    />
+    <Typography style={styles.title} variant="typoMedium" weight="medium">
+      {title}
+    </Typography>
+    <Typography style={styles.price} variant="typoSmall" weight="bold">
+      ${price.toFixed(2)}
+    </Typography>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'flex-start',
+  },
+  cover: {
+    width: 130,
+    height: 150,
+    borderRadius: radius.small,
+    marginBottom: 8,
+    backgroundColor: colors.grayBackground,
+    resizeMode: 'cover',
+  },
+  title: {
+    color: colors.textDark,
+    marginBottom: 4,
+  },
+  price: {
+    color: colors.primary,
+  },
+});
+
+export default BookCard;

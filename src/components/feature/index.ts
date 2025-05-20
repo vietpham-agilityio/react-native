012cd70aa77c9d982cb8152e3ba2cf9a3ba21069
Viet Xuan Pham <viet.pham@asnet.com.vn>
2025-05-20 16:19:51 +0700

    #25 Feature: Implement QuantityControlBar feature component

--- src/components/feature/index.ts
+++ src/components/feature/index.ts
@@ -2,3 +2,4 @@ export { default as AuthorCard } from './AuthorCard';
 export { default as BookCard } from './BookCard';
 export { default as VendorCard } from './VendorCard';
 export { default as Carousel } from './Carousel';
+export { default as QuantityControlBar } from './QuantityControlBar';
